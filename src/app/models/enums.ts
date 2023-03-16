ca9869d5780e2e45304c0e903d287749cc978eec
Shehbaz-alphai <116737522+Shehbaz-alphai@users.noreply.github.com>
2023-03-16 18:47:27 +0500

    commit

--- src/app/models/enums.ts
+++ src/app/models/enums.ts
@@ -187,7 +187,7 @@ export function propertySubTypeEnum(subTypeId: any) {
         case 16:
             return 'Project';
         default:
-            return "Unknown";
+            return '';
     }
 
 }
