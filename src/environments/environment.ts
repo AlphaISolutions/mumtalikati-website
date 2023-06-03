966c9ffc5ff2d483b52b1563c8adfbfba46e06ac
lyba-alphai <lyba@alphaisolutions.com>
2023-06-03 13:55:18 +0500

    removed a bug in env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
         storageBucket: '308729782341',
         messagingSenderId: '1:308729782341:web:84cf9407daca9cdc2b432c',
         appId: 'G-SGWT1DLVYQ'
-      }
+      },
     userBaseUrl:"https://api.mumtalikati.com/api",
     assetsBaseUrl : "http://d38tibxba17kdz.cloudfront.net"
 };
